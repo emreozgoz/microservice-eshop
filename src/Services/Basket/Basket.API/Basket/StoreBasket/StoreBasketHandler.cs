475398099a2d1fde2a777f52003fb0f57e5cf5fb
Emre ÖZGÖZ <emreozgoz@gmail.com>
2025-05-02 23:52:03 +0300

    up

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,24 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+    public class StoreBasletCommandValidator : AbstractValidator<StoreBasketCommand>
     {
+        public StoreBasletCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("Username is required");
+        }
+    }
+    public class StoreBasketHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand request, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = request.Cart;
+            //todo store baket in db
+            //todo update cache
+            return new StoreBasketResult("swn");
+        }
     }
 }
